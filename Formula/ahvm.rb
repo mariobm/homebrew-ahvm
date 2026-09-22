6dc40421af41163aca2d6487aabf5308720ad9a5
Mario <mario.balukcic1@gmail.com>
2026-09-22 08:46:48 +0200

    chore(release): Publish AHVM 0.3.7 (#16)

--- Formula/ahvm.rb
+++ Formula/ahvm.rb
@@ -1,19 +1,19 @@
 class Ahvm < Formula
   desc "Persistent Linux microVMs for coding agents"
   homepage "https://ahvm.app"
-  version "0.3.6"
+  version "0.3.7"
   license "LicenseRef-AHVM-Community-1.0"
   on_macos do
     depends_on arch: :arm64
     on_arm do
-      url "https://github.com/mariobm/agent-house/releases/download/v0.3.6/ahvm-client-0.3.6-darwin-aarch64.tar.gz"
-      sha256 "4ac67326979ec29b4ed105bde6d0d3fb3b671591c3b8821fb2f382d9ebbe6620"
+      url "https://github.com/mariobm/agent-house/releases/download/v0.3.7/ahvm-client-0.3.7-darwin-aarch64.tar.gz"
+      sha256 "f5b1a91091e2c90faa7069acf2fd4cdc8aa1649536d06574a0db64664add4bfe"
     end
   end
   on_linux do
     on_intel do
-      url "https://github.com/mariobm/agent-house/releases/download/v0.3.6/ahvm-client-0.3.6-linux-x86_64.tar.gz"
-      sha256 "5efd1acc4e317811e8f578e3598edc1fefc0c31f186097bc204c818d54119402"
+      url "https://github.com/mariobm/agent-house/releases/download/v0.3.7/ahvm-client-0.3.7-linux-x86_64.tar.gz"
+      sha256 "3d9bfb4272515c79994adb84ea6be57f36298b0efc88fbb4cdf68dc6ac156c68"
     end
   end
 
